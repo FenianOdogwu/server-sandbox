d4e766c30e5da40e6e60bb2ea2f2fde8a16d68ce
FenianOdogwu <mokoro95@yahoo.com>
2022-07-09 15:43:05 +0100

    added users routes folder and users.js

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express'); // get express package
 const app = express(); // creates app var for the server that allows us to configure server
 const mongoose = require ('mongoose');
+require('dotenv').config();
 
 
-mongoose.connect('mongodb://localhost:27017', {useNewUrlParser: true}) 
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true}) 
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Database'));
+
+
+app.use(express.json())
+
+//ROUTES
+const usersRouter = require('./routes/users');
+app.use('/users' , usersRouter) // url will use 'localhost:3000/users/'
+
 
 app.listen(3000, () => console.log('Server Started'));

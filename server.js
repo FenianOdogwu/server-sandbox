bf4a6b83ef766714a0dad44f04dd4c5084681025
FenianOdogwu <mokoro95@yahoo.com>
2022-07-23 00:20:26 +0100

    added view engine

--- server.js
+++ server.js
@@ -2,12 +2,22 @@ const express = require('express'); // get express package
 const app = express(); // creates app var for the server that allows us to configure server
 const mongoose = require ('mongoose');
 require('dotenv').config();
+var fs = require('fs')
+
+app.set('view engine', 'ejs'); // view engine to allow us to render html
 
 
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true}) 
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Database'));
+
+
+app.use('/', (req, res) => {
+  res.render('index')
+})
+
+
 
 //Middleware
 app.use(express.json())
